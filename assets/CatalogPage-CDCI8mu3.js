11d5b2c29f53a96064562bd4b364a26b5c9d9013
Eduard <sorokolietov@gmail.com>
2025-02-24 09:42:45 +0100

    Updates

---
+++ assets/CatalogPage-CDCI8mu3.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BXKLhMia.js";const e=()=>a.jsx("div",{children:"Catalog Page"});export{e as default};
---
+++ assets/FavoritesPage-w5atUE_I.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BXKLhMia.js";const t=()=>e.jsx("div",{children:"Favorites Page"});export{t as default};
